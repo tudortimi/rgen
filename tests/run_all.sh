a7bdad488b068d795f69261813a7312b73b5f95f
Tudor Timi <tudortimi@users.noreply.github.com>
2017-06-02 11:13:47 +0200

    added test that running with no arg should throw an error

---
+++ tests/run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+status=0
+
+for test in test__*; do
+    eval ./$test > logs/$test.log 2>&1
+    
+    if [[ "$?" == 0 ]]; then
+	echo "'$test' PASSED"
+    else
+	echo "'$test' FAILED"
+	status=1
+    fi
+done
+
+echo ''
+
+if [[ "$status" == 0 ]]; then
+    echo "Status is PASSED"
+else
+    echo "Status is FAILED"
+fi
---
+++ tests/test__no_arg__error.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+cd ..
+
+error=`mktemp`
+./rgen.py 2>$error
+
+if [[ "$?" == 0 ]]; then
+    exit 1
+fi
+
+# Emit captured output from 'rgen.py'
+cat < $error >&2
+
+if ! grep -q "too few arguments" $error; then
+    exit 1
+fi
+
+exit 0
